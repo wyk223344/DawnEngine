faf3c82e0c2c1f783640039c4f56d5ad67d1dbcb
wyk223344 <1594707439@qq.com>
2021-08-08 19:41:27 +0800

    pbr

--- Assets/Shaders/Lighting.hlsli
+++ Assets/Shaders/Lighting.hlsli
@@ -1,9 +1,58 @@
 
+const float PI = 3.14159265359;
 
 
+/*
+   D项：正态分布函数
+   估算在受到表面粗糙度的影响下，取向方向与中间向量一致的微平面的数量。这是用来估算微平面的主要函数。
+*/
+// Trowbridge-Reitz GGX正态分布函数
+float DistributionGGX(float3 N, float3 H, float roughness)
+{
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH * NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+/*
+   G项：几何函数
+   描述了微平面自成阴影的属性。当一个平面相对比较粗糙的时候，平面表面上的微平面有可能挡住其他的微平面从而减少表面所反射的光线。
+*/
+// Schlick-GGX
+float GeometrySchlickGGX(float NdotV, float k)
+{
+    float nom = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+
+// Smith’s method
+float GeometrySmith(float3 N, float3 V, float3 L, float k)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    // 观察方向 （几何遮蔽）
+    float ggx1 = GeometrySchlickGGX(NdotV, k);
+    // 光线方向  (几何阴影)
+    float ggx2 = GeometrySchlickGGX(NdotL, k);
+
+    return ggx1 * ggx2;
+}
 
 
-
+/*
+   F项：菲涅尔方程
+   菲涅尔方程描述的是在不同的表面角下表面所反射的光线所占的比率。
+*/
+// Fresnel Schlick近似
 float3 FresnelSchlick(float NdotV, float3 F0)
 {
 	return F0 + (1.0 - F0) * pow(max(1.0f - NdotV, 0.0f), 5.0f);
--- Assets/Shaders/PBRPS.hlsl
+++ Assets/Shaders/PBRPS.hlsl
@@ -1,4 +1,5 @@
 #include "Common.hlsli"
+#include "Lighting.hlsli"
 
 struct VertexOutput
 {
@@ -25,22 +26,52 @@ float4 main(VertexOutput input) : SV_TARGET
 	float metallic = MetallicMap.Sample(SamplerLinearClamp, input.uv).r;
 	float roughness = RoughnessMap.Sample(SamplerLinearClamp, input.uv).r;
 	float ao = AOMap.Sample(SamplerLinearClamp, input.uv).r;
+	// TODO: 法线贴图的采样器有些问题，之后待调整
+	float4 normalMapSample = NormalMap.Sample(SamplerLinearClamp, input.uv);
 
 	float3 positionWS = input.positionWS;
-	float3 normalWS = input.normalWS;
-	float3 tangentWS = input.tangentWS;
-	float3 viewPositionWS = normalize(CameraPosition - positionWS);
+	float3 normalWS = normalize(input.normalWS);
+	float3 tangentWS = normalize(input.tangentWS);
+	float3 V = normalize(CameraPosition - positionWS);
+	float3 N = NormalSampleToWorldSpace(normalMapSample.rgb, normalWS, tangentWS);
 
+	float3 F0 = float3(0.04f, 0.04f, 0.04f);
+	F0 = lerp(F0, albedo, metallic);
 
-	float3 ambient = albedo * ao * 0.03f;
+	float3 Lo = float3(0.0f, 0.0f, 0.0f);
 
+	for (int i = 0; i < DirectionalLightCount; i++)
+	{
+		LightData mainLight = LightDatas[i];
+		float3 lightColor = mainLight.Color;
+		float3 L = -normalize(mainLight.Direction);
+		float3 H = normalize(L + V);
+		float3 radiance = lightColor;
 
+		// Cook-Torrance BRDF
+		float D = DistributionGGX(N, H, roughness);
+		float G = GeometrySmith(N, V, L, roughness);
+		float3 F = FresnelSchlick(max(dot(H, V), 0.0f), F0);
+
+		float3 numerator = D * G * F;
+		float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.001; // 0.001 to prevent divide by zero.
+		float3 specular = numerator / denominator;
+
+		float3 kS = F;
+		float3 kD = float3(1.0f, 1.0f, 1.0f) - kS;
+		kD *= 1.0f - metallic;
+		float NdotL = max(dot(N, L), 0.0);
+		Lo += (kD * albedo / PI + specular) * radiance * NdotL;
+	}
+
+	float3 ambient = albedo * ao * 0.03f + Lo;
 
 	float3 color = ambient;
+
 	// HDR tonemapping
-	//color = color / (color + float3(1.0f, 1.0f, 1.0f));
+	color = color / (color + float3(1.0f, 1.0f, 1.0f));
 	// gamma correct
-	//color = pow(color, 1.0f / 2.2f);
+	color = pow(color, 1.0f / 2.2f);
 
 	return float4(color, 1.0f);
 }
